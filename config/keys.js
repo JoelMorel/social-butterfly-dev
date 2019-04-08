0c4da89dce4e8803fca6a9be04e73012aaa514f0
JoelMorel <jamkding@gmail.com>
2019-04-08 00:21:42 -0400

    added mongoURI & Google Client ID.

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dbadmin:scooby1234@butterfly-dev-a0n7w.mongodb.net/test?retryWrites=true",
+  googleClientID:
+    "625488887929-hscj381t76638ckbda3f1tebukdarhkm.apps.googleusercontent.com",
+  googleClientSecret: "lNAhh5CqpvQl1j1pRgR0CRUI"
+};
