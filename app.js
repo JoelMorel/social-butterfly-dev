a93409f2ca7cd8412f2473358135148d62c0b597
JoelMorel <jamkding@gmail.com>
2019-04-09 16:50:08 -0400

    added check for new user already in database. Added express-session and cookie-parser.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const cookie = require("cookie-parser");
+const session = require("express-session");
+
+// Load User Model
+require("./models/User");
 
 // Config Files
 require("./config/passport")(passport);
@@ -23,6 +28,20 @@ mongoose
 
 // Launch Express App
 const app = express();
+
+// Cookie parser and express session middle ware
+app.use(cookie());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("Success");
---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  googleID: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+});
+
+// Create collection and add schema
+mongoose.model("users", UserSchema);
